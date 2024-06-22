b03472fffd05290efcc99309c6574220e7f21ff5
shelly_goldblit <shelly_goldblit@dell.com>
2024-06-22 08:25:57 +0300

    add jest coverage badge

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,12 @@
 import type { JestConfigWithTsJest } from "ts-jest";
 
 const jestConfig: JestConfigWithTsJest = {
-  // [...]
+  coverageReporters: [
+    "clover",
+    "json-summary",
+    "lcov",
+    ["text", { skipFull: false }]
+  ],
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
